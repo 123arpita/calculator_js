6178c28228dbd27e308a2d3f5677ed8fc947fd3d
Arpita <guptaarpita159@gmail.com>
2022-09-24 10:51:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+let screen=document.getElementById('screen');
+buttons=document.querySelectorAll('button');
+let screenValue='';
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText=e.target.innerText;
+        console.log('Button Text is ', buttonText);
+        if(buttonText=='X'){
+            buttonText='*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if(buttonText=='C'){
+            screenValue="";
+            screen.value=screenValue;
+        }
+        else if(buttonText=='back'){
+            screenValue=screenValue.substring(0,screenValue.length-1);
+            screen.value=screenValue;
+        }
+        else if(buttonText=='='){
+            screen.value=eval(screenValue);
+        }
+        else if(buttonText=='sin'){
+            screenValue = Math.sin(screenValue);
+            screen.value = screenValue;
+        }
+        else if(buttonText=='cos'){
+            screenValue = Math.cos(screenValue);
+            screen.value = screenValue;
+        }
+        else if(buttonText=='tan'){
+            screenValue = Math.tan(screenValue);
+            screen.value = screenValue;
+        }
+        else{
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    })
+}
